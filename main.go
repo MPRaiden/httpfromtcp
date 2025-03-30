a051f9c066862f3d60749d0459dc503949db52b0
mpr <marinpericradan1@gmail.com>
2025-03-30 09:50:38 +0200

    chore: changed logic in main.go to read from tcp connection instead of from file

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
 )
 
 func getLinesChannel(f io.ReadCloser) <-chan string {
@@ -13,31 +13,41 @@ func getLinesChannel(f io.ReadCloser) <-chan string {
 	go func() {
 		defer f.Close()
 
-		buffer := make([]byte, 8)
+		buffer := make([]byte, 1024) // Use a larger buffer for efficiency
 		var currentLine []byte
 
 		for {
 			n, err := f.Read(buffer)
 			if err != nil {
-				if err == io.EOF {
-					// End of file reached
-					if len(currentLine) > 0 {
-						stringChannel <- string(currentLine)
-					}
-					break
+				if err == io.EOF && len(currentLine) > 0 {
+					stringChannel <- string(currentLine)
 				}
 				break
 			}
 
-			// Process the bytes read
-			for i := range buffer[:n] {
-				if buffer[i] == '\n' {
-					// Complete line found, send it to the channel
-					stringChannel <- string(currentLine)
-					currentLine = currentLine[:0] // Clear the slice
-				} else {
-					currentLine = append(currentLine, buffer[i])
+			// Add the new bytes to our current line
+			currentLine = append(currentLine, buffer[:n]...)
+
+			// Look for newlines and split the data accordingly
+			for {
+				i := 0
+				for i < len(currentLine) {
+					if currentLine[i] == '\n' {
+						// Send the line and remove it from currentLine
+						stringChannel <- string(currentLine[:i])
+						currentLine = currentLine[i+1:]
+						i = 0 // Start over with the new currentLine
+					} else {
+						i++
+					}
 				}
+				break // No more newlines found
+			}
+
+			// If we have data but no newline, send it anyway
+			if len(currentLine) > 0 {
+				stringChannel <- string(currentLine)
+				currentLine = currentLine[:0]
 			}
 		}
 
@@ -48,15 +58,27 @@ func getLinesChannel(f io.ReadCloser) <-chan string {
 }
 
 func main() {
-	file, err := os.Open("messages.txt")
+
+	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	linesChannel := getLinesChannel(file)
+	defer listener.Close()
 
-	for line := range linesChannel {
-		fmt.Printf("read: %s\n", line)
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Connection has been accepted")
+
+		linesChannel := getLinesChannel(connection)
+		for line := range linesChannel {
+			fmt.Printf("%s\n", line)
+		}
+
+		fmt.Println("Connection has been closed!")
 	}
-
 }
